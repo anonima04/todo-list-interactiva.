fe5fe570ffa7c19e0df037198790fc501dfc7789
Danna <dg5380523@gmail.com>
2025-10-29 14:49:57 -0500

    Initial commit: Set up HTML, JS, and CSS structure

---
+++ script.js
@@ -0,0 +1,8 @@
+// Base de datos de tareas en memoria
+const tasks = [];
+
+// Referencias a elementos del DOM
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
